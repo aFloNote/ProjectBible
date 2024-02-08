83f732b888a7b8e032d98b8a958b146e44427ac1
ck5150 <ck5150@proton.me>
2024-02-07 23:50:23 -0700

    backend work, switched to env variables for sensetive info. Created db connection function and storage init

--- OldTest/cmd/main.go
+++ OldTest/cmd/main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+  
+    _"github.com/lib/pq"
+	
 )
+
+
+
+
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello ff from Raven!")
 }
 
 func main() {
+
+
+
+// Construct the connecton string
+
+
 	http.HandleFunc("/", helloHandler)
 	http.ListenAndServe(":8080", nil)
 }
