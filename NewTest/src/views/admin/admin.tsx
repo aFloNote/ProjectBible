84a8b0c083a8ed48434ec49d1d3a95522c962950
ck5150 <ck5150@proton.me>
2024-03-23 20:39:47 -0600

    trying to fx not rebuilding

--- NewTest/src/views/admin/admin.tsx
+++ NewTest/src/views/admin/admin.tsx
@@ -8,7 +8,7 @@ import SermonAdminPage from "@/views/admin/sermonadmin/sermonadminpage";
 function Admin() {
   return (
    
-     
+     //somesomment
        <>
        <div className="flex flex-col h-screen">
          <div className="flex sticky top-0 pt-2 pb-1 pl-1 bg-background  border-b-2 dark:shadow-lg dark:shadow-blue-500/50 z-10">
